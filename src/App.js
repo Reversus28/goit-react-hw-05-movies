b70c1069c3f678dd5d926f0d8f22063170286ced
Oleksandr Yakovenko <mozg555666@gmail.com>
2021-08-21 11:35:56 +0300

    add finish code

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import { Route, Switch } from "react-router-dom";
+import { lazy, Suspense } from "react";
 import "./App.css";
 import AppBar from "./components/AppBar";
 import Container from "./components/Container";
 import Section from "./components/Section";
 import Navigation from "./components/Navigation";
-import HomeView from "./views/HomeView";
-import MovieDetailsView from "./views/MovieDetailsView";
-import MovieSearchView from "./views/MovieSearchView";
+
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import Loader from "react-loader-spinner";
+import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
 import PageNotFoundView from "./views/PageNotFoundView";
+
+const HomeView = lazy(() => import("./views/HomeView"));
+const MovieDetailsView = lazy(() => import("./views/MovieDetailsView"));
+const MovieSearchView = lazy(() => import("./views/MovieSearchView"));
 
 function App() {
   return (
@@ -19,37 +24,50 @@ function App() {
           <Navigation />
         </Container>
       </AppBar>
-      <Switch>
-        <Route path="/" exact>
-          <Section>
-            <Container>
-              <HomeView />
-            </Container>
-          </Section>
-        </Route>
+      <Suspense
+        fallback={
+          <Loader
+            type="ThreeDots"
+            color="tomato"
+            height={100}
+            width={100}
+            timeout={3000}
+            className="loader"
+          />
+        }
+      >
+        <Switch>
+          <Route path="/" exact>
+            <Section>
+              <Container>
+                <HomeView />
+              </Container>
+            </Section>
+          </Route>
 
-        <Route path="/movies" exact>
-          <Section>
-            <Container>
-              <MovieSearchView />
-            </Container>
-          </Section>
-        </Route>
+          <Route path="/movies" exact>
+            <Section>
+              <Container>
+                <MovieSearchView />
+              </Container>
+            </Section>
+          </Route>
 
-        <Route path="/movies/:movieId">
-          <Section>
-            <Container>
-              <MovieDetailsView />
-            </Container>
-          </Section>
-        </Route>
+          <Route path="/movies/:movieId">
+            <Section>
+              <Container>
+                <MovieDetailsView />
+              </Container>
+            </Section>
+          </Route>
 
-        <Route>
-          <Container>
-            <PageNotFoundView />
-          </Container>
-        </Route>
-      </Switch>
+          <Route>
+            <Container>
+              <PageNotFoundView />
+            </Container>
+          </Route>
+        </Switch>
+      </Suspense>
 
       <ToastContainer />
     </>
